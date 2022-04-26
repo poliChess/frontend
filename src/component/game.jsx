403043529f98b12a59b7949198668eed08248800
Puiu-Sebastian-Andrei <scorpionsebi@yahoo.com>
2022-04-26 11:35:22 +0300

    Added completed game interface.

--- src/component/game.jsx
+++ src/component/game.jsx
@@ -28,57 +28,45 @@ function Game() {
 
             
 
-            <div className="bg-green-600 flex justify-between">
-
-                {/* <div style={{position: "fixed", left: "3em", top: "3em"}}>
-
-                    <div className="m-1">
-                        <img className="m-auto" src={bishop} height="100px" width="100px" border="1px"/>
-                    </div>
-
-                    <div className="text-center">
-                        Player_1
-                    </div>
-
-                </div> */}
+            <div className="flex justify-between">
 
                 <div className="flex-grow flex-shrink-0">
 
-                    <div className="mt-2 ml-2 bg-blue-500 h-6">
+                    <div className="mt-4 ml-4 h-6">
                         <div>
                             <div style={{float: "left"}}><img src={horse} height="20px" width="20px" border="1px"/></div>
                             Poli<strong>Chess</strong>
                         </div>
                     </div>
 
-                    <div className="mt-20">
+                    <div className="mt-20 ml-4">
                             <img className="m-auto" src={bishop} height="100px" width="100px" border="1px"/>
                     </div>
 
-                    <div className="text-center">
+                    <div className="text-center font-mono ml-4">
                         Player_1
                     </div>
                         
                 </div>
 
-                <div className="bg-orange-600 flex flex-grow ml-44 mr-44 flex-shrink-0">
+                <div className="flex flex-grow ml-44 mr-44 flex-shrink-0">
 
-                    <div className="bg-white flex-grow self-center text-center">
-                        3:59
+                    <div className="font-['Helvetica'] underline underline-offset-4 flex-grow self-center text-center text-2xl text-button-1">
+                        <strong>03:59</strong>
                     </div>
 
-                    <div className="bg-green-400 flex-grow ml-10 mr-10 self-center">
+                    <div className="flex-grow ml-10 mr-10 self-center">
                             <img className="m-auto" src={clock1} height="140px" width="140px" border="1px"/>
                     </div>
 
-                    <div className="bg-white flex-grow self-center text-center">
-                        4:15
+                    <div className="font-['Helvetica'] underline underline-offset-4 flex-grow self-center text-center text-2xl text-red-600">
+                        <strong>04:15</strong>
                     </div>
                 </div>
 
                 <div className="flex-grow flex-shrink-0">
                         
-                    <div className="text-white text-right mt-2 mr-2 bg-red-500">
+                    <div className="text-white text-right mt-4 mr-4">
                         <Link to="/">
                             <button className="bg-button-2 hover:bg-purple-600 text-white w-24 h-6 
                             rounded-full shadow-lg hover:shadow-purple-600 shadow-button-2">
@@ -87,11 +75,11 @@ function Game() {
                         </Link>
                     </div>
 
-                    <div className="mt-20">
+                    <div className="mt-20 mr-4">
                             <img className="m-auto" src={king} height="100px" width="100px" border="1px"/>
                     </div>
 
-                    <div className="text-center">
+                    <div className="text-center font-mono mr-4">
                             Player_2
                     </div>
                         
@@ -100,13 +88,13 @@ function Game() {
                 
             </div>
 
-            <div className="bg-blue-600 flex mt-5">
+            <div className="flex mt-5">
 
-                <div className="bg-purple-600 flex-grow flex items-center shrink-0">
+                <div className="flex-grow flex items-center shrink-0">
                     
-                    <div className="bg-red-500 flex-grow m-7">
+                    <div className="mr-11 bg-white flex-grow m-16 rounded-2xl border-b-black border-t-black border-4 shadow-xl shadow-button-1 hover:shadow-purple-600">
 
-                    <div className="bg-orange-100 flex items-center m-1">
+                    <div className="bg-white flex items-center m-1">
 
                         <div className="m-auto">
 
@@ -119,7 +107,7 @@ function Game() {
                         </div>
 
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                         <div className="m-auto">
 
@@ -132,7 +120,7 @@ function Game() {
                         </div>
 
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                         <div className="m-auto">
 
@@ -145,7 +133,7 @@ function Game() {
                         </div>
 
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                         <div className="m-auto">
 
@@ -158,7 +146,7 @@ function Game() {
                         </div>
 
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                         <div className="m-auto">
 
@@ -176,7 +164,7 @@ function Game() {
                 </div>
 
                 <div className="flex-grow content-center flex shrink-0">
-                    <table className="chess-board m-auto">
+                    <table className="chess-board m-auto shadow-2xl shadow-black hover:shadow-purple-600">
                         <tbody>
                             <tr>
                                 <th></th>
@@ -364,11 +352,11 @@ function Game() {
                     </table>
                 </div>
 
-                <div className="bg-purple-600 flex-grow flex items-center shrink-0">
+                <div className="flex-grow flex items-center shrink-0">
                     
-                    <div className="bg-red-500 flex-grow m-7">
+                    <div className="ml-11 bg-white flex-grow m-16 rounded-2xl border-b-black border-t-black border-4 shadow-xl shadow-red-600 hover:shadow-purple-600">
 
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                             <div className="m-auto">
                             
@@ -376,12 +364,12 @@ function Game() {
                                     <div className="text-center">
                                         0 X
                                     </div>
-                                    <img className="" src={white_pawn} height="50px" width="50px" border="1px"/>
+                                    <img className="" src={black_pawn} height="50px" width="50px" border="1px"/>
                                 </div>
                             </div>
                             
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                             <div className="m-auto">
                             
@@ -389,12 +377,12 @@ function Game() {
                                     <div className="text-center">
                                         0 X
                                     </div>
-                                    <img className="" src={white_rook} height="50px" width="50px" border="1px"/>
+                                    <img className="" src={black_rook} height="50px" width="50px" border="1px"/>
                                 </div>
                             </div>
                             
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                             <div className="m-auto">
                             
@@ -402,12 +390,12 @@ function Game() {
                                     <div className="text-center">
                                         0 X
                                     </div>
-                                    <img className="" src={white_knight} height="50px" width="50px" border="1px"/>
+                                    <img className="" src={black_knight} height="50px" width="50px" border="1px"/>
                                 </div>
                             </div>
                             
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                             <div className="m-auto">
                             
@@ -415,12 +403,12 @@ function Game() {
                                     <div className="text-center">
                                         0 X
                                     </div>
-                                    <img className="" src={white_bishop} height="50px" width="50px" border="1px"/>
+                                    <img className="" src={black_bishop} height="50px" width="50px" border="1px"/>
                                 </div>
                             </div>
                             
                         </div>
-                        <div className="bg-orange-100 flex items-center m-1">
+                        <div className="bg-white flex items-center m-1">
 
                             <div className="m-auto">
                             
@@ -428,7 +416,7 @@ function Game() {
                                     <div className="text-center">
                                         0 X
                                     </div>
-                                    <img className="" src={white_queen} height="50px" width="50px" border="1px"/>
+                                    <img className="" src={black_queen} height="50px" width="50px" border="1px"/>
                                 </div>
                             </div>
                             
