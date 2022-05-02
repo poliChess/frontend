f6326424ef89771816b98f1d8bf2a7387d8c19f9
Puiu-Sebastian-Andrei <scorpionsebi@yahoo.com>
2022-05-02 18:12:58 +0300

    Format update.

--- src/component/decoration.jsx
+++ src/component/decoration.jsx
@@ -3,26 +3,26 @@ import king_bg from "../pictures/backgrounds/king_bg.png";
 import pieces1_bg from "../pictures/backgrounds/pieces1_bg.png";
 import pieces2_bg from "../pictures/backgrounds/pieces2_bg.png";
 
-
 function Decoration(screen) {
+  var images = [],
+    quotes = [],
+    index_quote,
+    index_image = 0;
 
-    var images = [], quotes = [], index_quote, index_image = 0;
+  images[0] = horse_bg;
+  images[1] = king_bg;
+  images[2] = pieces1_bg;
 
-images[0] = horse_bg;
-images[1] = king_bg;
-images[2] = pieces1_bg;
+  index_image = Math.floor(Math.random() * images.length);
 
-index_image = Math.floor(Math.random() * images.length);
+  quotes[0] = '" Chess is the gymnasium of the mind "';
+  quotes[1] = '" Chess is beautiful enough to waste your life for "';
+  quotes[2] = '" Nobody ever won a chess game by resigning "';
 
-quotes[0] = '" Chess is the gymnasium of the mind "';
-quotes[1] = '" Chess is beautiful enough to waste your life for "';
-quotes[2] = '" Nobody ever won a chess game by resigning "';
-
-index_quote = Math.floor(Math.random() * quotes.length);
+  index_quote = Math.floor(Math.random() * quotes.length);
 
   return (
     <div className="flex bg-white">
-
       {screen}
 
       <div className="bg-right-bg flex-row flex-grow hidden md:flex basis-[40%]">
@@ -32,7 +32,6 @@ index_quote = Math.floor(Math.random() * quotes.length);
                     */}
 
           <div className="bg-right-bg text-4xl w-72 ml-20 mt-20">
-            
             {quotes[index_quote]}
           </div>
 
