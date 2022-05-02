f97c3e027331316bd9258282203ab986589e5997
Puiu-Sebastian-Andrei <scorpionsebi@yahoo.com>
2022-05-02 18:11:30 +0300

    Added randomised feature for quotes and backgrounds

--- src/component/decoration.jsx
+++ src/component/decoration.jsx
@@ -1,10 +1,28 @@
 import horse_bg from "../pictures/backgrounds/horse_bg.png";
-import { Link } from "react-router-dom";
+import king_bg from "../pictures/backgrounds/king_bg.png";
+import pieces1_bg from "../pictures/backgrounds/pieces1_bg.png";
+import pieces2_bg from "../pictures/backgrounds/pieces2_bg.png";
+
 
 function Decoration(screen) {
+
+    var images = [], quotes = [], index_quote, index_image = 0;
+
+images[0] = horse_bg;
+images[1] = king_bg;
+images[2] = pieces1_bg;
+
+index_image = Math.floor(Math.random() * images.length);
+
+quotes[0] = '" Chess is the gymnasium of the mind "';
+quotes[1] = '" Chess is beautiful enough to waste your life for "';
+quotes[2] = '" Nobody ever won a chess game by resigning "';
+
+index_quote = Math.floor(Math.random() * quotes.length);
+
   return (
     <div className="flex bg-white">
-        
+
       {screen}
 
       <div className="bg-right-bg flex-row flex-grow hidden md:flex basis-[40%]">
@@ -14,13 +32,14 @@ function Decoration(screen) {
                     */}
 
           <div className="bg-right-bg text-4xl w-72 ml-20 mt-20">
-            " Chess is the gymnasium of the mind "
+            
+            {quotes[index_quote]}
           </div>
 
           <div className="opacity-20">
             <img
               className="h-full"
-              src={horse_bg}
+              src={images[index_image]}
               alt="Horse background"
               style={{ position: "fixed", right: "-12em", bottom: "-6em" }}
             />
