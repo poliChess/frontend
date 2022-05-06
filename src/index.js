22c8aec6327b262b932fe1d15f464b77bbe6425b
mihneabuz <mihneabuzatu88@gmail.com>
2022-05-06 19:16:54 +0300

    fixed typo and missing packages

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 
-import state from './state/store';
+import store from './state/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
