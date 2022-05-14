7db99ae8f8bcfcb0faa40838fd5c0da26666d31c
Puiu-Sebastian-Andrei <scorpionsebi@yahoo.com>
2022-05-14 14:16:31 +0300

    Format code.

--- src/component/screen/profile2.jsx
+++ src/component/screen/profile2.jsx
@@ -48,15 +48,15 @@ function Profile2() {
     <div className="bg-white flex-row flex-grow justify-center items-center h-screen p-8 z-10 w-132 relative">
         <Title />
         
-           <div className='bg-red-600 mt-6 flex'> 
-
+        <div className='bg-red-600 mt-6 flex'> 
+        
             <div>
                 <img
-              className="m-auto"
-              src={avatar}
-              height="100px"
-              width="100px"
-              border="1px"
+                className="m-auto"
+                src={avatar}
+                height="100px"
+                width="100px"
+                border="1px"
                 />
             </div>
 
@@ -69,79 +69,46 @@ function Profile2() {
                         faraonul@piramide.com
                     </div>
                 </div>
-
-                {/* <div className='bg-purple-600'>
-                    faraonul@piramide.com
-                </div> */}
             </div>
 
             <div>
-            <img
-              className="m-auto"
-              src={avatar}
-              height="100px"
-              width="100px"
-              border="1px"
+                <img
+                className="m-auto"
+                src={avatar}
+                height="100px"
+                width="100px"
+                border="1px"
                 />
             </div>
               
-          </div> 
+        </div> 
 
           
-        <div className="bg-decoration-bg flex flex-grow flex-col basis-[40%] items-center text-white">
-          
+        <div className="bg-pink-600 flex flex-grow flex-col basis-[40%] items-center text-white">
 
-
-         <div>
-           <div className='bg-transparent rounded-full flex'>
-                <div className='bg-blue-500 flex-grow text-center rounded-tl-full'>
-                    Played: 15
-                </div>
+            <div>
+                <div className='bg-transparent rounded-full flex'>
+                    <div className='bg-blue-500 flex-grow text-center rounded-tl-full'>
+                        Played: 15
+                    </div>
                 
-                <div className='bg-blue-500 flex-grow text-center rounded-tr-full'>
-                    Won: 10
-                </div>     
-            </div>
-          <div className='bg-white h-96 w-96 flex-row overflow-y-scroll'>
-            {/* <div className='bg-yellow-600'>
-                adsda
-            </div>
-            <div className='bg-purple-600'>
-                adsda
-            </div>
-            <div className='bg-blue-600'>
-                adsda
-            </div>
-            <div className='bg-yellow-600'>
-                adsda
-            </div>
-            <div className='bg-purple-600'>
-                adsda
-            </div>
-            <div className='bg-blue-600'>
-                adsda
-            </div>
-            <div className='bg-yellow-600'>
-                adsda
-            </div>
-            <div className='bg-purple-600'>
-                adsda
-            </div>
-            <div className='bg-blue-600'>
-                adsda
-            </div> */}
+                    <div className='bg-blue-500 flex-grow text-center rounded-tr-full'>
+                        Won: 10
+                    </div>     
+                </div>
+
+                <div className='bg-white h-96 w-96 flex-row overflow-y-scroll'>
             
-            {Match(189, 'Decebal', 'Defeat', loss)}
-            {Match(149, 'Ghiu', 'Victory', win)}
-            {Match(169, 'Boicea', 'Defeat', loss)}
-            {Match(119, 'HeHeByeBoy', 'Victory', win)}
-            {Match(119, 'Ardeleanu', 'Victory', win)}
-            {Match(119, 'Iohannis', 'Victory', win)}
-            {Match(119, 'Popcorn', 'Victory', win)}
-          </div>
+                    {Match(189, 'Decebal', 'Defeat', loss)}
+                    {Match(149, 'Ghiu', 'Victory', win)}
+                    {Match(169, 'Boicea', 'Defeat', loss)}
+                    {Match(140, 'HeHeByeBoy', 'Victory', win)}
+                    {Match(130, 'Ardeleanu', 'Victory', win)}
+                    {Match(120, 'Iohannis', 'Victory', win)}
+                    {Match(100, 'Popcorn', 'Victory', win)}
+                </div>
           
-          
-         </div>
+            </div>
         </div>
     </div>
   );
