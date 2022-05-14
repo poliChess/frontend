5f6a6a70c1e0571568038613b9584defb82d30c4
TudiSP <tudor.hermenean@gmail.com>
2022-05-14 21:22:46 +0300

    Added Circular Progress Bar for Sebi chan

--- src/component/screen/profile2.jsx
+++ src/component/screen/profile2.jsx
@@ -12,6 +12,37 @@ import apiclient from '../../utils/apiclient.js';
 import User from '../user';
 
 
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+
+function ProgressCircle(percentage) {
+    return (<CircularProgressbar
+        value={percentage}
+        text={`${percentage}%`}
+        styles={buildStyles({
+            // Rotation of path and trail, in number of turns (0-1)
+            rotation: 0.25,
+
+            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+            strokeLinecap: 'butt',
+
+            // Text size
+            textSize: '16px',
+
+            // How long animation takes to go from one percentage to another, in seconds
+            pathTransitionDuration: 0.5,
+
+            // Can specify path transition in more detail, or remove it entirely
+            // pathTransition: 'none',
+
+            // Colors
+            pathColor: `rgba(62, 152, 199, ${percentage / 100})`,
+            textColor: '#f88',
+            trailColor: '#d6d6d6',
+            backgroundColor: '#3e98c7',
+        })}
+    />);
+}
+
 /**
  * 
  * @param {int} time seconds elapsed
@@ -106,10 +137,12 @@ function Profile2() {
                     {Match(130, 'Ardeleanu', 'Victory', win)}
                     {Match(120, 'Iohannis', 'Victory', win)}
                     {Match(100, 'Popcorn', 'Victory', win)}
+                    
                 </div>
           
             </div>
         </div>
+        {ProgressCircle(70)}
     </div>
   );
 
