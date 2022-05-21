a3d65e2b3e699fe4a7010c7539f361f8de889258
mihneabuz <mihneabuzatu88@gmail.com>
2022-05-22 01:45:56 +0300

    added game slice to redux

--- src/state/store.js
+++ src/state/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice';
+import gameSlice from './gameSlice';
 
 export default configureStore({
   reducer: {
     user: userSlice,
+    game: gameSlice
   },
 })
