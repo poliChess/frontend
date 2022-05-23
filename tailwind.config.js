fdf7500f1d864e643abbe3fdd3d22ee6e4674409
mihneabuz <mihneabuzatu88@gmail.com>
2022-05-23 16:33:03 +0300

    added colors for board to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
         'decoration-bg': '#D6DEE2',
         'main-color': '#3499DB',
         'secondary-color': '#164362',
-        'accent-color': '#702D57'
+        'accent-color': '#702D57',
+        'board-white': '#fafafa',
+        'board-black': '#C6CED2',
+        'board-white-highlight': '#467d9f',
+        'board-black-highlight': '#2b668c'
       },
       spacing: {
         '70': '18rem',
@@ -18,8 +22,6 @@ module.exports = {
     },
   },
   plugins: [
-    // require('tailwind-animation-delay'),
-    // require('tailwindcss-animate'),
     require('tailwind-scrollbar-hide')
   ],
 }
